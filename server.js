93fc198cd93566da4e406257d55fe79a825ab2b9
Sator Arepo <satorarepo@tuta.io>
2020-01-04 22:06:41 -0600

    Connected everything

--- server.js
+++ server.js
@@ -2,23 +2,30 @@ const PORT = 3000;
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const Post = require("./models/post");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/api/posts", (rq, rs) => rs.json([
-		{
-			username: "johndoe",
-			body: "node rocks!"
-		}
-	])
-);
+app.get("/", (rq, rs) => rs.sendFile(__dirname + "/posts.html"));
 
-app.post("/api/posts", (rq, rs) => {
-	console.log("Post received!");
-	console.log(rq.body.username);
-	console.log(rq.body.body);
-	rs.status(201);
+app.get("/api/posts", (rq, rs, next) => {
+	Post.find((e, posts) => {
+		if (e) return next(e);
+		rs.json(posts);
+	});
+});
+
+app.post("/api/posts", (rq, rs, next) => {
+	const post = new Post({
+		username: rq.body.username,
+		body: rq.body.body
+	});
+	post.save((e, post) => {
+		if (e) return next(e);
+		rs.status(201);
+		rs.json(post);
+	});
 });
 
 app.listen(PORT, function () {
