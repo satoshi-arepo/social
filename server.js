990a2045d292947c50b7d5248618a57ea241a0c6
Sator Arepo <satorarepo@tuta.io>
2020-01-04 23:18:17 -0600

    Rearranged the code in several filesmpty message aborts the commit.

--- server.js
+++ server.js
@@ -2,32 +2,14 @@ const PORT = 3000;
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const Post = require("./models/post");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (rq, rs) => rs.sendFile(__dirname + "/posts.html"));
+app.use("/", require("./controllers/static.js"));
+app.use("/api/posts", require("./controllers/api/posts.js"));
 
-app.get("/api/posts", (rq, rs, next) => {
-	Post.find((e, posts) => {
-		if (e) return next(e);
-		rs.json(posts);
-	});
-});
 
-app.post("/api/posts", (rq, rs, next) => {
-	const post = new Post({
-		username: rq.body.username,
-		body: rq.body.body
-	});
-	post.save((e, post) => {
-		if (e) return next(e);
-		rs.status(201);
-		rs.json(post);
-	});
-});
-
-app.listen(PORT, function () {
+app.listen(PORT, () => {
 	console.log("Server listening on", PORT)
 });
